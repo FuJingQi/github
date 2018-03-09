8eaf3adfad4b0e64177a0a551aec020d985ce6f9
付景奇 <944697693@qq.com>
2018-03-09 15:14:00 +0800

    My First Commit

---
+++ src/main/java/User.java
@@ -0,0 +1,14 @@
+public class User {
+    public int id;
+
+    public User() {
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
